83e3679d3308b3d6b51038dcbdd5458086c86e26
Francis Bui <47166254+Francis-Bui@users.noreply.github.com>
2021-12-01 11:38:32 -0500

    Selection fixed

--- BuiArray/BuiArray/Form1.cs
+++ BuiArray/BuiArray/Form1.cs
@@ -13,29 +13,21 @@ namespace BuiArray
     {
 
         int spc = 110;  // space between each tile
-        int initPosX;
         int prevCordX = -110;
         int prevCordY = 0;
 
-        Point tempCord;   // temporary SWAP location 
         Point[] initCord = new Point[9];  // holds the original tiles locations (only need 9, but want to start at 1)
         Rectangle[] rect = new Rectangle[9];
+        Boolean[] chk = new Boolean[9];
 
-
-        bool pass = false;
         bool clicked = false;
         bool genFlag = false;
 
-        private Rectangle main;
-
         public Form1()
         {
-            main = new Rectangle(0, 0, this.Width, this.Height);
             InitializeComponent();
             
             this.BackColor = Color.Aqua;
-
-            //store INITIAL locations of buttons -- use for checkWin()
 
         }
 
@@ -46,7 +38,6 @@ namespace BuiArray
 
         private void CanvasPaint(object sender, PaintEventArgs e)
         {
-
                 for (int i = 0; i < 9; i++)
                 {
                     if (i == 3 || i == 6)
@@ -58,17 +49,11 @@ namespace BuiArray
                     if (genFlag == false)
                     {
                         rect[i].Location = new Point(prevCordX + spc, prevCordY);
-
                         rect[i].Size = new Size(100, 100);
+                        e.Graphics.FillRectangle(Brushes.Black, rect[i]);
                     }
 
-                    if (clicked == false)
-                        e.Graphics.FillRectangle(Brushes.Black, rect[i]);
-                    else
-                        e.Graphics.FillRectangle(Brushes.White, rect[i]);
-
                     initCord[i] = rect[i].Location;
-
                     prevCordX = rect[i].Location.X;
                     prevCordY = rect[i].Location.Y;
 
@@ -76,29 +61,48 @@ namespace BuiArray
                     {
                         genFlag = true;
                     }
-   
-            }
+
+                    if (genFlag == true)
+                    {
+                        foreach (Rectangle Value in rect)
+                        {
+                            if (chk[i] == false)
+                            {
+                                e.Graphics.FillRectangle(Brushes.Black, rect[i]);
+                            }
+                            else if (chk[i] == true)
+                            {
+                                if (i >= 0 && i <= 5 && (chk[i + 3] == true || chk[i + 1] == true))
+                                    e.Graphics.FillRectangle(Brushes.White, rect[i]);
+                                else if (i >= 3 && i <= 8 && (chk[i - 3] == true || chk[i - 1] == true))
+                                    e.Graphics.FillRectangle(Brushes.White, rect[i]);
+                                else if (i >= 1 && chk[i - 1] == true)
+                                    e.Graphics.FillRectangle(Brushes.White, rect[i]);
+                                else if (i <= 8 && chk[i + 1] == true)
+                                    e.Graphics.FillRectangle(Brushes.White, rect[i]);
+                            }
+                        }
+                    }
+                }
+
+                
         }
 
         private void RectMouseDown(object sender, MouseEventArgs e)
         {
+            Point relativeMouse = this.PointToClient(Cursor.Position);
             int i = 0;
 
-            while(i < 9)
+            foreach (Rectangle Value in rect)
             {
-                Point relativeMouse = this.PointToClient(Cursor.Position);
-
-                if ((relativeMouse.X <= (initCord[i].X + rect[i].Width) && relativeMouse.X >= initCord[i].X) && (relativeMouse.Y <= (initCord[i].Y + rect[i].Height) && relativeMouse.Y >= initCord[i].Y))
+                if ((relativeMouse.X <= (initCord[i].X + rect[i].Width) && relativeMouse.X >= initCord[i].X) && (relativeMouse.Y <= (initCord[i].Y + rect[i].Height) && relativeMouse.Y >= initCord[i].Y) && (chk.Count(c => c) < 2))
                 {
-                    clicked = true;
+                    chk[i] = true;
                 }
-                else 
+                else
                 {
-                    clicked = false;
+                    i++;
                 }
-
-                i++;
-
             }
 
             this.Refresh();
